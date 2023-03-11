7b1657bf5f2bbece883db7c3a435bdf94d301235
jayvie2007 <jayvief.deleon@gmail.com>
2023-03-12 01:14:43 +0800

    My First C++ Program

---
+++ Myfirstprogram/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(){
+        std::cout << "hello pukang" << std::endl; /*<< std::endl; means new line*/ 
+        std::cout << "Number 1 ";
+        std::cout << "Number 2 \n" ; /* \n alternative new line*/
+        std::cout << "Number 3 ";
+
+        /*print your name 10x*/
+        for (int i = 0; i <= 10; i++){
+            std::cout << "Jayvie De Leon" << std::endl;
+        }
+        return 0;
+}       
