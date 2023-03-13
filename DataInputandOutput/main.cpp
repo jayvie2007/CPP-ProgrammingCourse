5b386e5cf6c6c80eb383db505959a69217ee3061
jayvie2007 <jayvief.deleon@gmail.com>
2023-03-13 19:26:27 +0800

    Fixed skipping getline data

--- DataInputandOutput/main.cpp
+++ DataInputandOutput/main.cpp
@@ -4,7 +4,7 @@
 int main(){
     //Print Data
     // cout means C output
-    /*
+
     int age{10};        
     std::string name;
     std:: cout << "Hello C++" << std::endl;
@@ -19,12 +19,14 @@ int main(){
     //cin means c input
     std:: cin >> name >> age;
     std:: cout << "Hello " << name << " and welcome to C++. Your age is: " << age << std::endl;
-    */
+
     //in order to store string line with spaces use std::getline
     std:: string full_name;
     int age2;
     std:: cout << "Please input your full name! " << std::endl;
-    std:: getline(std::cin, full_name);
+    // include std::cin.ignore(); when inputting multiple data using std::getline() to avoid getline to be skipped
+    std::cin.ignore();
+    std::getline(std::cin, full_name);
     std:: cout << "Please input your age! " << std::endl;
     std:: cin >> age2;
 
@@ -32,8 +34,3 @@ int main(){
 
     return 0;
 }
-
-/*Error that I have encountered... 
-When I removed the block comment and compile it it does run but
-before entering the getline command it immediatele skips to age2
-*/
